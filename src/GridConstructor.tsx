1823b066dd1d96c677f41eb6db3d1f752c5055f9
FarbrorGarbo <ask.jaho@gmail.com>
2019-10-15 20:29:13 +0200

    Small changes to the boilerplate app.

---
+++ src/GridConstructor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GridConstructor: React.FC = () => {
+  return (
+    <div className="App">
+      <p>Work in progress. Let's see if we can get this app up on GitHub pages</p>
+    </div>
+  );
+}
+
+export default GridConstructor;
