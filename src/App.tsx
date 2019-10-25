fb00a09b443cf4f2df0f028558759cf95351fe7c
FarbrorGarbo <ask.jaho@gmail.com>
2019-10-25 19:53:13 +0200

    Added first outlines of perspective settings and simple 3d gizmo and cube drawn on canvas reflecting the perspective.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,103 @@
-import React from 'react';
+import React from "react";
 import GridConstructor from './GridConstructor';
-import './App.css';
+import "./App.css";
+
+// const useSettings: React.FC<{ init?: string }> = (init) => {
+//   const localStorageData: string | null = localStorage.getItem("grid");
+//   const [value, setValue] = React.useState<any>(
+//       !localStorageData ? {} || init
+//     );
+//   React.useEffect(
+//     () => {
+//       localStorage.setItem("grid", JSON.stringify(value));
+//     }
+//   )
+//   return [value, setValue];
+// }
+
+type GenericNumberInputProps = {
+	label: string;
+	min: number;
+	max: number;
+	step: number;
+	value: number;
+	returnValue(val: number): void;
+}
+
+const GenericNumberInput: React.FC<GenericNumberInputProps> = (props) => {
+	const inputRef = React.useRef<HTMLInputElement>(null);
+	const [value, setValue] = React.useState(props.value);
+
+	const updateValue = () => {
+		let  newValue = parseInt(inputRef.current!.value === "" ? "0" : inputRef.current!.value);
+		if (newValue < props.min) newValue = props.min;
+		else if (newValue > props.max) newValue = props.max;
+		setValue(newValue);
+	}
+
+	const {returnValue} = props;
+
+	React.useEffect( () => { returnValue(value) }, [returnValue, value] );
+
+	return (
+		<label>{props.label}:
+			<input
+				ref={inputRef}
+				type="number"
+				min={props.min}
+				max={props.max}
+				step={props.step}
+				onChange={() => updateValue()}
+				value={value.toString()}
+			/>
+		</label>
+	);
+}
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <GridConstructor />
-    </div>
-  );
+	// Show/hide settings
+	const [showSettings, toggle] = React.useState(false);
+
+	function toggleSettings (event: any) {
+		event.preventDefault();
+		toggle(!showSettings);
+	}
+
+	// Settings
+	const [rotation, setRotation] = React.useState(25);
+	const [elevation, setElevation] = React.useState(20);
+	const [distance, setDistance] = React.useState(1000);
+	const [picturePlane, setPicturePlane] = React.useState(1000);
+	const [offsetH, setOffsetH] = React.useState(Math.floor(window.innerWidth/2));
+	const [offsetV, setOffsetV] = React.useState(Math.floor(window.innerHeight/2));
+
+	const handleRotation = (value: number) => { setRotation(value) };
+	const handleElevation = (value: number) => { setElevation(value) };
+	const handleDistance = (value: number) => { setDistance(value) };
+	const handlePicturePlane = (value: number) => { setPicturePlane(value) };
+	const handleOffsetH = (value: number) => { setOffsetH(value) };
+	const handleOffsetV = (value: number) => { setOffsetV(value) };
+
+	// React.useEffect( () => { console.log(rotation)}, [rotation]);
+	// React.useEffect( () => { console.log(elevation)}, [elevation]);
+
+	return (
+		<div className="App">
+			<GridConstructor settings={{rotation: rotation, elevation: elevation, distance: distance, picturePlane: picturePlane, offsetH: offsetH, offsetV: offsetV}} />
+			{showSettings &&
+			<div className={"settings"}>
+				<button onClick={toggleSettings}>{showSettings ? "Close" : "Settings"}</button>
+				<h2>Perspektive Settings</h2>
+				<GenericNumberInput label={"Rotation"} min={0} max={359} step={5} value={rotation} returnValue={(val) => handleRotation(val)} />
+				<GenericNumberInput label={"Elevation"} min={0} max={90} step={5} value={elevation} returnValue={(val) => handleElevation(val)} />
+				<GenericNumberInput label={"Distance"} min={0} max={999999} step={50} value={distance} returnValue={(val) => handleDistance(val)} />
+				<GenericNumberInput label={"Distance to Picture Plane"} min={0} max={999999} step={50} value={picturePlane} returnValue={(val) => handlePicturePlane(val)} />
+				<GenericNumberInput label={"Offset Horisontal"} min={0} max={999999} step={5} value={offsetH} returnValue={(val) => handleOffsetH(val)} />
+				<GenericNumberInput label={"Offset Vertical"} min={0} max={999999} step={5} value={offsetV} returnValue={(val) => handleOffsetV(val)} />
+			</div>}
+			{!showSettings && <button onClick={toggleSettings}>{showSettings ? "Close" : "Settings"}</button>}
+		</div>
+	);
 }
 
 export default App;
--- src/GridConstructor.tsx
+++ src/GridConstructor.tsx
@@ -1,11 +1,146 @@
-import React from 'react';
+import React from "react";
 
-const GridConstructor: React.FC = () => {
-  return (
-    <div className="App">
-      <p>Work in progress. Let's see if we can get this app up on GitHub pages</p>
-    </div>
-  );
+type Settings = {
+    rotation: number,
+    elevation: number,
+    distance: number,
+    picturePlane: number,
+    offsetH: number,
+    offsetV: number
+}
+
+type ViewProps = {
+    settings: Settings
+}
+
+type Vector = {
+	x: number,
+	y: number,
+	z: number
+}
+
+const degrees_to_radians = (degrees: number) => {
+  return degrees * (Math.PI / 180);
+}
+
+const projcet = (settings: Settings, point: Vector) => {
+	const rot = degrees_to_radians(settings.rotation);
+	const elev = degrees_to_radians(settings.elevation);
+	const n = point.x * Math.sin(rot) + point.y * Math.cos(rot);
+	const a = point.x * Math.cos(rot) - point.y * Math.sin(rot);
+	const b = n * Math.cos(elev) - point.z * Math.sin(elev);
+	const c = n * Math.sin(elev) + point.z * Math.cos(elev);
+	const h = settings.picturePlane * a / (settings.distance + b) + settings.offsetH;
+	const v = settings.picturePlane * c / (settings.distance + b) + settings.offsetV;
+	return [h, v];
+}
+
+const GridConstructor: React.FC<ViewProps> = ({ settings }: ViewProps) => {
+	const canvasElm = React.useRef<HTMLCanvasElement>(null);
+	const [canvasSize, setCanvasSize] = React.useState({width: window.innerWidth, height: window.innerHeight});
+	
+	React.useEffect(() => {
+		const canvas = canvasElm.current;
+		// console.log(settings);
+
+		if (canvas) {
+			canvas.width = canvasSize.width;
+			canvas.height = canvasSize.height;
+
+			const ctx = canvas.getContext("2d");
+			if (!!ctx) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				ctx.strokeStyle = "#f00";
+
+				const xLines = [
+					[{x:0, y:0, z:0}, {x:500, y:0, z:0}],
+					[{x:480, y:0, z:5}, {x:500, y:0, z:0}]
+				]
+				xLines.forEach(line => {
+					const calcPointA = projcet(settings, line[0]);
+					const calcPointB = projcet(settings, line[1]);
+					ctx.beginPath();
+					ctx.moveTo(calcPointA[0], canvas.height - calcPointA[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+					ctx.stroke();
+				});
+
+				ctx.strokeStyle = "#0f0";
+				const yLines = [
+					[{x:0, y:0, z:0}, {x:0, y:500, z:0}],
+					[{x:5, y:480, z:0}, {x:0, y:500, z:0}]
+				]
+				yLines.forEach(line => {
+					const calcPointA = projcet(settings, line[0]);
+					const calcPointB = projcet(settings, line[1]);
+					ctx.beginPath();
+					ctx.moveTo(calcPointA[0], canvas.height - calcPointA[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+					ctx.stroke();
+				});
+
+				ctx.strokeStyle = "#00f";
+				const zLines = [
+					[{x:0, y:0, z:0}, {x:0, y:0, z:500}],
+					[{x:5, y:0, z:480}, {x:0, y:0, z:500}]
+				]
+				zLines.forEach(line => {
+					const calcPointA = projcet(settings, line[0]);
+					const calcPointB = projcet(settings, line[1]);
+					ctx.beginPath();
+					ctx.moveTo(calcPointA[0], canvas.height - calcPointA[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+					ctx.stroke();
+				});
+
+				ctx.strokeStyle = "#aaa";
+				ctx.beginPath();
+
+				const cubeLines = [
+					[{x:-200, y:-200, z:-200}, {x:200, y:-200, z:-200}],
+					[{x:-200, y:200, z:-200}, {x:200, y:200, z:-200}],
+					[{x:-200, y:-200, z:200}, {x:200, y:-200, z:200}],
+					[{x:-200, y:200, z:200}, {x:200, y:200, z:200}],
+
+					[{x:-200, y:-200, z:-200}, {x:-200, y:-200, z:200}],
+					[{x:200, y:-200, z:-200}, {x:200, y:-200, z:200}],
+					[{x:200, y:200, z:-200}, {x:200, y:200, z:200}],
+					[{x:-200, y:200, z:-200}, {x:-200, y:200, z:200}],
+					
+					[{x:-200, y:-200, z:200}, {x:-200, y:200, z:200}],
+					[{x:200, y:-200, z:200}, {x:200, y:200, z:200}],
+					[{x:-200, y:-200, z:-200}, {x:-200, y:200, z:-200}],
+					[{x:200, y:-200, z:-200}, {x:200, y:200, z:-200}]
+				]
+				cubeLines.forEach(line => {
+					const calcPointA = projcet(settings, line[0]);
+					const calcPointB = projcet(settings, line[1]);
+					ctx.moveTo(calcPointA[0], canvas.height - calcPointA[1]);
+					ctx.lineTo(calcPointB[0], canvas.height - calcPointB[1]);
+				});
+				ctx.stroke();
+			}
+		}
+
+		const onResize = () => {
+			setCanvasSize({width: window.innerWidth, height: window.innerHeight});
+		}
+
+		window.addEventListener('resize', onResize);
+
+		return () => {
+			window.removeEventListener('resize', onResize);
+		}
+	},
+	[settings, canvasSize]
+	);
+
+  	return (
+		<canvas className={"canvas"} ref={canvasElm} />
+  	);
 }
 
 export default GridConstructor;
