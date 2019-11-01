f32b7833db6868c2ecea50cb94d36fe9af592be9
FarbrorGarbo <ask.jaho@gmail.com>
2019-11-01 19:26:38 +0100

    Updates

--- precache-manifest.e8a4634646814d6132b89b91902809bc.js
+++ precache-manifest.e8a4634646814d6132b89b91902809bc.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f86f598269aace0a18ad095e49760401",
+    "revision": "ac110f4d95133cbaff41c1493564af72",
     "url": "/GridConstructor/index.html"
   },
   {
-    "revision": "c12f300b7f482f16fdce",
-    "url": "/GridConstructor/static/css/main.de346e89.chunk.css"
+    "revision": "3dc44c9650b295e2cbfc",
+    "url": "/GridConstructor/static/css/main.b1bd372e.chunk.css"
   },
   {
     "revision": "6e9d4baf020860f38d3f",
     "url": "/GridConstructor/static/js/2.354d63f7.chunk.js"
   },
   {
-    "revision": "c12f300b7f482f16fdce",
+    "revision": "3dc44c9650b295e2cbfc",
     "url": "/GridConstructor/static/js/main.90062fa7.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GridConstructor/precache-manifest.8bf9cb1401709b0f0c739cd32f54cfaa.js"
+  "/GridConstructor/precache-manifest.e8a4634646814d6132b89b91902809bc.js"
 );
 
 self.addEventListener('message', (event) => {
